b69e43b7c11c8d38fcc708375038a284e984c994
Maria Gambarte <maria.gambarte@globant.com>
2022-10-22 17:28:33 -0300

    add buy product to contract

--- contracts/CryptoShop.sol
+++ contracts/CryptoShop.sol
@@ -3,72 +3,109 @@ pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 
 contract CryptoShop {
-    event NewProduct(uint256 index);
+    event NewProduct(uint256 index, CatalogItem item);
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
-    struct CatalogItem{
-      string description;
-      uint256 price; //ether values are in wei, one ether = 1e18wei
-      uint stock;
+    mapping(address => uint256) balances;
+    mapping(uint256 => CatalogItem) public itemMapped;
+
+    struct CatalogItem {
+        string description;
+        uint256 price; //ether values are in wei, one ether = 1e18wei
+        uint stock;
     }
 
     // An address type variable is used to store ethereum accounts.
     address public owner;
+    uint256 public totalSupply = 5;
 
     CatalogItem[] catalog;
 
     constructor() {
         owner = msg.sender;
+        balances[msg.sender] = totalSupply;
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        console.log(balances[owner]);
+        return balances[account];
     }
 
     function getCatalogLength() external view returns (uint256) {
         return catalog.length;
     }
 
-    function getCatalogItemAt(uint16 index) external view returns (string memory description, uint256 price, uint stock) {
-        require(index < catalog.length , 'Item not found');
+    function getCatalogItemAt(uint16 index)
+        external
+        view
+        returns (
+            string memory description,
+            uint256 price,
+            uint stock
+        )
+    {
+        require(index < catalog.length, "Item not found");
         CatalogItem storage item = catalog[index]; //A: storage porque no lo vamos a modificar ni devolver
         return (item.description, item.price, item.stock);
     }
 
-    // // Publish an offer or service 
-    function addProduct(string calldata description, uint256 price, uint stock) external returns (uint256) {
-      require(price > 0 , 'Price must be greater than 0');
-      require(stock > 0 , 'Stock must be greater than 0');
-      //TODO: revisar descripción - string convertido en bytes?
+    //Publish an offer or service
+    function addProduct(
+        string memory description,
+        uint256 price,
+        uint stock
+    ) external returns (uint256) {
+        require(price > 0, "Price must be greater than 0");
+        require(stock > 0, "Stock must be greater than 0");
+        //TODO: revisar descripción - string convertido en bytes?
 
-      CatalogItem memory item = CatalogItem(description, price, stock); //A: memory porque lo estamos creando- se usa para cambiar y devolver
-      catalog.push(item);
+        CatalogItem memory item = CatalogItem(description, price, stock); //A: memory porque lo estamos creando- se usa para cambiar y devolver
+        catalog.push(item);
 
-      uint256 index = catalog.length -1;
-      emit NewProduct(index);  //A: return index
-      return index;
+        uint256 index = catalog.length - 1;
+        emit NewProduct(index, item); //A: return index
+        return index;
     }
 
     // buy a product receiving the product id
-    function buyProduct(address account, uint256 productId) external pure {
-      require(account != address(0), "Account is the zero address");
-      require(productId <= 0, "Invalid product id");
+    function buyProduct(uint256 productId,uint256 price,uint32 units,address to) external {
+        require(productId >= 0, "Invalid product id");
+        require(units > 0, "Not enough stock to complete transaction"); //TODO: compare units with existing stock
+        require(balances[msg.sender] >= price, "Not enough tokens");
 
+        //TODO: reduce stock amount
+
+        // Transfer the amount.
+        balances[msg.sender] -= price;
+        balances[to] += price;
+
+        // Notify off-chain applications of the transfer.
+        emit Transfer(msg.sender, to, price);
     }
 
     // check if payment is correct
-    function checkPayment(address account, uint256 purchaseId) external pure returns (bool) {
-      require(account != address(0), "Account is the zero address");
-      require(purchaseId <= 0, "Invalid product id");
+    function checkPayment(address account, uint256 purchaseId)
+        external
+        pure
+        returns (bool)
+    {
+        require(account != address(0), "Account is the zero address");
+        require(purchaseId <= 0, "Invalid product id");
 
-      return true;
+        return true;
     }
 
     //Resolve if we have to pay to seller or return balance to buyer
-    function resolveTransaction(uint complainId, bool payToSeller) external pure {
-      require(complainId > 0, "Invalid complain id");
-      
-      if (payToSeller != true){
-        // revert purchase
-      } else {
-        // pay to seller
-      }
+    function resolveTransaction(uint complainId, bool payToSeller)
+        external
+        pure
+    {
+        require(complainId > 0, "Invalid complain id");
+
+        if (payToSeller != true) {
+            // revert purchase
+        } else {
+            // pay to seller
+        }
     }
-
-
 }
--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ const { expect } = require("chai");
 
 describe("CryptoShop Contract", function () {
   async function deployCryptoShopFixture() {
-    const [owner] = await ethers.getSigners();
+    const [owner, address1, address2] = await ethers.getSigners();
 
     const CryptoShopContract = await ethers.getContractFactory("CryptoShop");
 
@@ -12,41 +12,77 @@ describe("CryptoShop Contract", function () {
     await CryptoShop.deployed();
 
     // Fixtures can return anything you consider useful for your tests
-    return { CryptoShop, CryptoShopContract, owner};
+    return { CryptoShop, CryptoShopContract, owner, address1, address2 };
   }
 
-  async function addProduct(CryptoShop) {
-    const tx = await CryptoShop.addProduct("my producto", 20, 30);
+  async function addProduct(CryptoShop, description, price, quantity) {
+    const tx = await CryptoShop.addProduct(description, price, quantity);
     const receipt = await tx.wait();
 
-    //DEBUG:console.log({events: JSON.stringify(receipt.events, null, 1)});
+    //DEBUG:console.log({events: JSON.stringify(receipt.events[0].transactionHash)});
 
     const itemIndex = receipt.events[0].args[0];
 
-    return {itemIndex}
+    const itemDetail = receipt.events[0].args[1];
+
+    const productId = receipt.events[0].transactionHash;
+
+    return { itemIndex, itemDetail, productId }
   }
 
-  it("Catalog should start empty", async function () {
-    const  { CryptoShop } = await deployCryptoShopFixture();
+  describe("Product catalog", function () {
+    it("Catalog should start empty", async function () {
+      const { CryptoShop } = await deployCryptoShopFixture();
 
-    const catalogLength = await CryptoShop.getCatalogLength();
-    expect(catalogLength).to.equal(0);
-    await expect(CryptoShop.getCatalogItemAt(1)).to.be.revertedWith("Item not found");
-  });
+      const catalogLength = await CryptoShop.getCatalogLength();
+      expect(catalogLength, ("Catalog is not empty")).to.equal(0);
+      await expect(CryptoShop.getCatalogItemAt(1)).to.be.revertedWith("Item not found");
+    });
 
-  it("Item added must show in catalog", async function () {
-    const { CryptoShop } = await deployCryptoShopFixture();
+    it("Item added must show in catalog", async function () {
+      const { CryptoShop } = await deployCryptoShopFixture();
 
-    const itemIndex0 = await addProduct(CryptoShop);
-    console.log({itemIndex0});
-    expect(itemIndex0.itemIndex.eq(0), ("Item not found")).to.be.true; //TODO: buscar o agregar una función para loguear bien- ethereum waffle
+      const itemIndex0 = await addProduct(CryptoShop, "My description", 20, 30);
+      const itemIndex1 = await addProduct(CryptoShop, "My description", 20, 30); //FOR THIS TEST ONLY, order is non deterministic
+      await expect(itemIndex0.itemIndex.eq(0), (`Item at position ${itemIndex0.itemIndex} does not equal ${0}`)).to.be.true;
+      await expect(itemIndex1.itemIndex.eq(1), (`Item at position ${itemIndex0.itemIndex} does not equal ${1}`)).to.be.true;
+    });
 
-    const itemIndex1 = await addProduct(CryptoShop); //FOR THIS TEST ONLY, order is non deterministic
-    console.log({itemIndex1});
-    expect(itemIndex1.itemIndex.eq(1)).to.be.true;
-  }); 
+    it("Item added without price or stock should fail", async function () {
+      const { CryptoShop } = await deployCryptoShopFixture();
+
+      await expect(addProduct(CryptoShop, "My description", 0, 30)).to.be.revertedWith("Price must be greater than 0");
+      await expect(addProduct(CryptoShop, "My description", 20, 0)).to.be.revertedWith("Stock must be greater than 0");
+    });
     // TO DO: check item data eg: description not empty 
-  it("Buying item should decrease stock by 1", async function () {});
-  it("Buying item without stock should fail", async function () {});
-  it("Buying item below price should fail", async function () {});
+  })
+
+  describe("Buy product", function () {
+    it("Buying item should decrease stock by 1", async function () { });
+
+    it("Buying product with correct transaction should emit product id", async function () {
+      const { CryptoShop, owner, address2 } = await deployCryptoShopFixture();
+      const { itemDetail, productId } = await addProduct(CryptoShop, "My description", 5, 1);
+
+      await expect(CryptoShop.buyProduct(productId, itemDetail.price, 1, address2.address))
+        .to.emit(CryptoShop, "Transfer").withArgs(owner.address, address2.address, itemDetail.price);
+    })
+
+    it("Buying item without enough stock should fail", async function () {
+      const { CryptoShop, owner } = await deployCryptoShopFixture();
+      const { itemDetail, productId } = await addProduct(CryptoShop, "My description", 5, 1);
+
+      await expect(CryptoShop.buyProduct(productId, itemDetail.price, 0, owner.address)).to.be.revertedWith("Not enough stock to complete transaction");
+    });
+
+    it("Buying item below price should fail", async function () {
+      const { CryptoShop, owner } = await deployCryptoShopFixture();
+      const { itemDetail, productId } = await addProduct(CryptoShop, "My description", 20, 10);
+
+      //DEBUG:console.log(itemDetail);
+
+      await expect(CryptoShop.buyProduct(productId, itemDetail.price, 1, owner.address)).to.be.revertedWith("Not enough tokens");
+    });
+  })
+
 });
